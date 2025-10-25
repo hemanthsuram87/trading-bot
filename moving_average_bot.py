6d155e7a022ea2bfd02864d58fc1786eee27f88f
Hemanth Suram <hemanthsuram87@gmail.com>
2025-10-25 00:34:06 -0400

    Remove unused import for alpaca_trade_api

--- moving_average_bot.py
+++ moving_average_bot.py
@@ -3,7 +3,6 @@ import time
 import pandas as pd
 import numpy as np
 import requests
-import alpaca_trade_api as tradeapi
 from datetime import datetime, timedelta
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
