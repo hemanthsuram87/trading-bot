495141e70d8db3e84fd5d0875631db24ca29f392
Hemanth Suram <hemanthsuram87@gmail.com>
2025-10-27 00:09:35 -0400

    Update moving_average_bot.py

--- moving_average_bot.py
+++ moving_average_bot.py
@@ -22,14 +22,12 @@ from ta.volatility import AverageTrueRange
 import yfinance as yf
 
 # ================= CONFIG =================
-ALPACA_KEY = "PK237O7I63OSG4RKO32AQMCQVG"
-ALPACA_SECRET = "EFez3L8WnR3DouNoXRYk2LEsCGpXejCTwd9ebUopxt1e"
-ALPACA_BASE_URL = os.getenv("ALPACA_BASE_URL", "https://paper-api.alpaca.markets")
+ALPACA_KEY = os.getenv("ALPACA_KEY")
+ALPACA_SECRET = os.getenv("ALPACA_SECRET")
+ALPACA_BASE_URL = "https://paper-api.alpaca.markets"
 # Telegram config
-TELEGRAM_TOKEN = "8475528816:AAFntgwGkp9jW5mVVnaX1MHGtM4kjPfnvC8"
-TELEGRAM_CHAT_ID = "7862318105"
-GOOGLE_CREDS_FILE = "service_account.json"
-api_key="2iGFEJ1lGax2deRX426NEBiDpCc0dGrO"
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 api = tradeapi.REST(ALPACA_KEY, ALPACA_SECRET, base_url=ALPACA_BASE_URL)
 
 EST = pytz.timezone("US/Eastern")
